891fecbf2a291c51cdd4fb6055069a5d1ebc5994
kc0836 <kc0836@gmail.com>
2019-03-31 19:10:35 +0800

    second commit

---
+++ gameBoard.hpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <iomanip>
+#include <cstddef>
+
+using namespace std;
+
+class Gameboard
+{
+    char gameSpace[4][4];
+
+  public:
+    Gameboard();                                        // initialize the board with '-' in all 16 spaces
+    void setGameSpace(int row, int column, char value); // x, y or '-' in each game space
+    char getGameSpace(int row, int column);
+    int fourInRow();  // four 'x's in any row 'wins'
+    void printInfo(); // print the game board in a 4x4 matrix
+};
+
+Gameboard::Gameboard()
+{
+    for (size_t i = 0; i < 4; i++)
+        for (size_t j = 0; j < 4; j++)
+            gameSpace[i][j] = '-';
+}
+
+void Gameboard::setGameSpace(int row, int column, char value)
+{
+    gameSpace[row][column] = value;
+}
+
+char Gameboard::getGameSpace(int row, int column)
+{
+    return gameSpace[row][column];
+}
+
+int Gameboard::fourInRow()
+{
+    // check for every row and looking for four 'x's.
+    int check = 0;
+    for (size_t i = 0; i < 4; i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            if (gameSpace[i][j] == 'x')
+                check++;
+        }
+        if (check == 4)
+            return 1;
+    }
+
+    return 0;
+}
+
+void Gameboard::printInfo()
+{
+    for (size_t i = 0; i < 4; i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            cout << gameSpace[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
